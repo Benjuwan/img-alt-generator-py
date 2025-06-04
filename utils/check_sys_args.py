a5d2bae97ef0a7f76f03db22b0596dee4e003794
benjuwan <benjuwan.banquet12@gmail.com>
2025-06-04 20:37:40 +0900

    fix: 処理結果をエクセルファイルに保存する追加実装及びリファクタリング、各種調整作業.

--- utils/check_sys_args.py
+++ utils/check_sys_args.py
@@ -1,24 +1,26 @@
-import sys # コマンドライン引数を扱う
+import sys  # コマンドライン引数を扱う
 
 
 # URLパスをチェックするプライベートメソッド
 def _check_url_path(target_url):
     # print(target_url[0])
-    has_http = 'http' in target_url[0]
-    has_https = 'https' in target_url[0]
-    has_slash = '/' in target_url[0]
+    has_http = "http" in target_url[0]
+    has_https = "https" in target_url[0]
+    has_slash = "/" in target_url[0]
     if has_http or has_https or has_slash:
         return target_url[0]
 
-    sys.exit('URLを指定してください')
+    sys.exit("URLを指定してください")
 
 
 # 1. はじめに、モジュールの主要な処理を関数にまとめる
 def check_sys_args():
     if len(sys.argv) != 2:
         # （コマンドライン引数が1つでない場合は）引数に指定した文字列を表示して処理終了
-        this_filename = sys.argv[0] # 先頭（1つ目）はファイル名
-        sys.exit(f"'{this_filename}'に必要な引数は「1つ」ですが、指定された引数は「{len(sys.argv) - 1 if len(sys.argv) == 1 else len(sys.argv)}つ」です")
+        this_filename = sys.argv[0]  # 先頭（1つ目）はファイル名
+        sys.exit(
+            f"'{this_filename}'に必要な引数は「1つ」ですが、指定された引数は「{len(sys.argv) - 1 if len(sys.argv) == 1 else len(sys.argv)}つ」です"
+        )
 
     # コマンドライン引数が1つの場合（正常処理パターン）
     except_filename_args = sys.argv[1:]  # 先頭以外の全ての引数
